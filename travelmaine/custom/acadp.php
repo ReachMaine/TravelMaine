0fbe96dffc76d552f4683bde7f16ea3f991b1811
linda@reachmaine.com <linda@reachmaine.com>
2019-05-23 15:45:54 -0400

    working

--- travelmaine/custom/acadp.php
+++ travelmaine/custom/acadp.php
@@ -15,3 +15,13 @@ function gotravel_before_content ( $in_id) {
 
 
 } // end of function gotravel_before_content
+
+
+/**
+ * Enables the  custom fields box in listing edit screen.
+ */
+function gotravel_listing_supports() {
+  add_post_type_support('acadp_listings', 'custom-fields');
+}
+//add_action( 'plugins_loaded', 'gotravel_listing_supports' );
+add_action('init', 'gotravel_listing_supports', 100);
