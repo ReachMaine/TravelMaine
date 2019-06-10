dd1ea4b61e3a9642bd98573f81707a522147578c
linda@reachmaine.com <linda@reachmaine.com>
2019-06-10 10:47:41 -0400

    simplify single listing display
    using page.php as basis instead of single.php

--- travelmaine/single-acadp_listings.php
+++ travelmaine/single-acadp_listings.php
@@ -1,79 +1,45 @@
 <?php
-/* mods
-	24May19 zig - copy single.php and remove the featured image/postmeta at top.
-*/
+/***  template for single listing ***/
+/* 10Jun19 - zig - copy page templates & rename for single listing
+								 - remove conditionals for $is_page_builder_used */
 
 get_header();
 
-$show_default_title = get_post_meta( get_the_ID(), '_et_pb_show_title', true );
-
-$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
+//$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 
 ?>
 
 <div id="main-content">
-	<?php
-		if ( et_builder_is_product_tour_enabled() ):
-			// load fullwidth page in Product Tour mode
-			while ( have_posts() ): the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
-					<div class="entry-content">
-					<?php
-						the_content();
-					?>
-					</div> <!-- .entry-content -->
 
-				</article> <!-- .et_pb_post -->
-
-		<?php endwhile;
-		else:
-	?>
 	<div class="container">
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<div class="entry-content">
+						<div class="entry-content">
 					<?php
-						do_action( 'et_before_content' );
-
 						the_content();
 
-						wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
 					?>
 					</div> <!-- .entry-content -->
-					<div class="et_post_meta_wrapper">
-					<?php
-					if ( et_get_option('divi_468_enable') === 'on' ){
-						echo '<div class="et-single-post-ad">';
-						if ( et_get_option('divi_468_adsense') !== '' ) echo et_core_intentionally_unescaped( et_get_option('divi_468_adsense'), 'html' );
-						else { ?>
-							<a href="<?php echo esc_url(et_get_option('divi_468_url')); ?>"><img src="<?php echo esc_attr(et_get_option('divi_468_image')); ?>" alt="468" class="foursixeight" /></a>
-				<?php 	}
-						echo '</div> <!-- .et-single-post-ad -->';
-					}
-				?>
 
-					<?php if (et_get_option('divi_integration_single_bottom') !== '' && et_get_option('divi_integrate_singlebottom_enable') === 'on') echo et_core_intentionally_unescaped( et_get_option('divi_integration_single_bottom'), 'html' ); ?>
 
-					<?php
-						if ( ( comments_open() || get_comments_number() ) && 'on' === et_get_option( 'divi_show_postcomments', 'on' ) ) {
-							comments_template( '', true );
-						}
-					?>
-					</div> <!-- .et_post_meta_wrapper -->
 				</article> <!-- .et_pb_post -->
 
 			<?php endwhile; ?>
+
 			</div> <!-- #left-area -->
 
 			<?php get_sidebar(); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
-	<?php endif; ?>
+
+
+
 </div> <!-- #main-content -->
 
 <?php
